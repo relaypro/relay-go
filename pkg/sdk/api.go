e9a6ee0f5b81a133daf3eabf1ca826bd6760dd78
Ashley Godwin <agodwin@relaypro.com>
2022-09-09 14:30:20 -0400

    Test comment for start interaction

--- pkg/sdk/api.go
+++ pkg/sdk/api.go
@@ -169,7 +169,13 @@ func (wfInst *workflowInstance) GetSourceUri(startEvent StartEvent) string {
     return startEvent.Trigger.Args.SourceUri
 }
 
-// This is a start interaction function.  It starts an interaction.
+// Starts an interaction with the user.  Triggers an INTERACTION_STARTED event
+// and allows the user to interact with the device via functions that require an 
+// interaction URN.
+//
+// target (target): the device that you would like to start an interaction with.
+// name (str): a name for your interaction.
+// options (optional): can be color, home channel, or input types. Defaults to None.
 func (wfInst *workflowInstance) StartInteraction(sourceUri string, name string) StartInteractionResponse {
     id := makeId()
     target := makeTargetMap(sourceUri)
